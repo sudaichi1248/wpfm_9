fe8384cc3f2653b2146a25ff1e88c2caf4650d08
KASAI-LIFEBOOK\kasai <kasai@abit.co.jp>
2022-08-24 18:23:16 +0900

    起動時、パラメータ不正でも正常起動に　【暫定】WPFM_settingParameter.isInvalid

--- firmware/src/init.c
+++ firmware/src/init.c
@@ -79,8 +79,8 @@ void WPFM_initializeApplication(void)
     {
         DEBUG_UART_printlnString("parameter read error.");
     }
-    //if (true)
-    if (WPFM_settingParameter.isInvalid)
+    if (true)
+//kasai    if (WPFM_settingParameter.isInvalid)
     {
         // Set default values
         DEBUG_UART_printlnString("Set default parameter.");
