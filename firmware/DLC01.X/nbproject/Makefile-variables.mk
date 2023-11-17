561390a917705a22dd97efdf868be70d565d00b5
kasai-surface5\admin <kasai-kk@live.jp>
2023-11-18 08:54:08 +0900

    DLC00->DLC02へ

--- firmware/DLC01.X/nbproject/Makefile-variables.mk
+++ firmware/DLC01.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # atsamd21g18a_default configuration
 CND_ARTIFACT_DIR_atsamd21g18a_default=dist/atsamd21g18a_default/production
-CND_ARTIFACT_NAME_atsamd21g18a_default=DLC00.X.production.hex
-CND_ARTIFACT_PATH_atsamd21g18a_default=dist/atsamd21g18a_default/production/DLC00.X.production.hex
+CND_ARTIFACT_NAME_atsamd21g18a_default=DLC01.X.production.hex
+CND_ARTIFACT_PATH_atsamd21g18a_default=dist/atsamd21g18a_default/production/DLC01.X.production.hex
