0bc986fe503a119bb476c4f80530d73327fa1d9c
KASAI-LIFEBOOK\kasai <kasai@abit.co.jp>
2023-11-29 23:59:45 +0900

    DLC0とDLC2を追加

---
+++ firmware/DLC02.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# atsamd21g18a_default configuration
+CND_ARTIFACT_DIR_atsamd21g18a_default=dist/atsamd21g18a_default/production
+CND_ARTIFACT_NAME_atsamd21g18a_default=DLC01.X.production.hex
+CND_ARTIFACT_PATH_atsamd21g18a_default=dist/atsamd21g18a_default/production/DLC01.X.production.hex
