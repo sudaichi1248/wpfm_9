3fa0d08dbc918ed99ac10ec6b0ecd38a6c620e23
KASAI-LIFEBOOK\kasai <kasai@abit.co.jp>
2023-12-22 11:17:42 +0900

    00.B2へ

--- firmware/src/version.h
+++ firmware/src/version.h
@@ -1,10 +1,10 @@
 #ifndef  BOARD_PROTOTYPE2
-#define MAIN_VERSION "Ver 00.B1 "
+#define MAIN_VERSION "Ver 00.B2 "
 #else
 #ifdef _Version2
 #define MAIN_VERSION "Ver 02.00 "
 #else
-#define MAIN_VERSION "Ver 00.B1 "
+#define MAIN_VERSION "Ver 00.B2 "
 #endif
 #endif
 extern	char 	_Main_version[32];
