ba73288f4483deb3b3d2661cb1ba48f090ea67b4
kasai-surface5\admin <kasai-kk@live.jp>
2024-01-29 11:07:29 +0900

    00.B4へUpしなおしのしなおし

--- firmware/src/version.h
+++ firmware/src/version.h
@@ -1,10 +1,6 @@
-#ifndef  BOARD_PROTOTYPE2
-#define MAIN_VERSION "Ver 00.B4 "
-#else
 #ifdef _Version2
 #define MAIN_VERSION "Ver 02.00 "
 #else
 #define MAIN_VERSION "Ver 00.B4 "
-#endif
 #endif
 extern	char 	_Main_version[32];
